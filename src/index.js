1d4524d9db393b676d687d368512218331f5119d
Sam Poder <sam.r.poder@gmail.com>
2022-12-30 11:44:58 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+let color = "black";
+
+io.on("connection", (socket) => {
+  socket.emit("color", color);
+
+  socket.on("emoji", (emoji) => {
+    socket.broadcast.emit("emoji", emoji);
+  });
+
+  socket.on("color", (selectedColor) => {
+    color = selectedColor;
+    socket.broadcast.emit("color", selectedColor);
+  });
+
+  socket.on("message", (message) => {
+    io.emit("messageForReview", message);
+  });
+
+  socket.on("acceptMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("refresh", () => {
+    io.emit("refresh");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "./views" });
+});
+
+app.get("/display", (req, res) => {
+  res.sendFile("display.html", { root: "./views" });
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile("admin.html", { root: "./views" });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
